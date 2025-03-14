49a8b6c448908a5ba7cad16957d36b78d0672453
mmirobar17.alumnes <marti.miro.06@gmail.com>
2025-03-14 10:19:30 +0100

    Classes filles d'acces

---
+++ src/prog2/model/CamiAsfaltat.java
@@ -0,0 +1,22 @@
+package prog2.model;
+
+public class CamiAsfaltat extends Acces {
+    private float asfalt;
+
+    // Constructor
+    public CamiAsfaltat(boolean accessibilitat, boolean estat, Allotjament desti, float asfalt) {
+        super(accessibilitat, estat, desti);
+
+        this.setAsfalt(asfalt);
+    }
+
+    // Getters
+    public float getAsfalt() {
+        return asfalt
+    }
+
+    // Setters
+    public void setAsfalt(float asfalt_) {
+        this.asfalt = asfalt_;
+    }
+}
