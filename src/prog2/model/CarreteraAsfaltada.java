af6145531610220122a88f8f1ef9be25c7661e5a
mmirobar17.alumnes <marti.miro.06@gmail.com>
2025-03-14 10:29:52 +0100

    Classes filles de cami asfaltat i cami terra

---
+++ src/prog2/model/CarreteraAsfaltada.java
@@ -0,0 +1,21 @@
+package prog2.model;
+
+public class CarreteraAsfaltada extends CamiAsfaltat{
+    private float pesMaxim;
+
+    // Constructor
+    public CarreteraAsfaltada(boolean accessibilitat, boolean estat, Allotjament desti, float asfalt, float pesMaxim) {
+        super(accessibilitat, estat, desti, asfalt);
+
+        this.setPesMaxim(pesMaxim);
+    }
+
+    // Getters
+    public float getPesMaxim() {
+        return pesMaxim;
+    }
+    // Setters
+    public void setPesMaxim(float pesMaxim) {
+        this.pesMaxim = pesMaxim;
+    }
+}
---
+++ src/prog2/model/CarreteraTerra.java
@@ -0,0 +1,24 @@
+package prog2.model;
+
+public class CarreteraTerra  extends  CamiTerra {
+    private  float amplada;
+
+    // Constructor
+
+    public CarreteraTerra(boolean accessibilitat, boolean estat, Allotjament desti, float longitud, float amplada) {
+        super(accessibilitat, estat, desti, longitud)
+
+        this.setAmplada(float amplada);
+    }
+
+    // Getters
+    public float getAmplada() {
+        getAmplada();
+    }
+
+    // Setters
+    public void setAmplada(float amplada) {
+        this.amplada = amplada;
+    }
+
+}
