f4ee30c6a5e5b0f4307cf595ed3737c99534106b
EntrepaDeFuet <orioljb1999@gmail.com>
2025-03-26 20:08:19 +0100

    Excepcio camping fet, easy peasy

---
+++ Test/prog2/model/AllotjamentTest.java
@@ -0,0 +1,14 @@
+package prog2.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AllotjamentTest {
+
+    @org.junit.jupiter.api.Test
+    void getId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEstat() {
+    }
+}
