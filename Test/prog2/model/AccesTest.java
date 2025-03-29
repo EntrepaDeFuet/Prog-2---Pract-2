37586ee96d5fd854cddc6fd7a73384eafdbfe808
mrt <mmirobar17@alumnes.ub.edu>
2025-03-29 18:35:05 +0100

    AllotjamentTest fet

---
+++ Test/prog2/model/AccesTest.java
@@ -0,0 +1,28 @@
+package prog2.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccesTest {
+
+    @BeforeEach
+    void setUp() {}
+
+    @Test
+    void afegirAllotjament() {
+    }
+
+    @Test
+    void obrirAcces() {
+    }
+
+    @Test
+    void tancarAcces() {
+    }
+
+    @Test
+    void isAccessibilitat() {
+    }
+}
